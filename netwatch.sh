372a6d6fc1c1344ecfbdda92b1890e1e14564244
Jason Spohn <atari911@gmail.com>
2016-12-23 11:19:32 -0800

    Update netwatch.sh

--- netwatch.sh
+++ netwatch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-NET=$HOME/./net.sh
+## This is basically a 'watch' wrapper for the 'net.sh' script.
+
+NET=$HOME/./net.sh	# most likely you will need to make this right or change it
 
 if [ -z $1 ]
 	then watch -n 1 $NET
