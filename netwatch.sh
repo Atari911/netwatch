4f0b6b63b0911457f4f3295e88925bfc6ab9c69e
Jason Spohn <atari911@gmail.com>
2016-12-23 11:21:31 -0800

    Update netwatch.sh

--- netwatch.sh
+++ netwatch.sh
@@ -6,6 +6,6 @@ NET=$HOME/./net.sh	# most likely you will need to make this right or change it
 
 ## Wrap the script in a watch command unless there is an argument in which case run 'netstat' with said argument.
 if [ -z $1 ]
-	then watch -n 1 $NET
-	else netstat $1
+	then watch -n 1 $NET	# run the 'net.sh' script and refresh every second
+	else netstat $1		# if an argument is present then append it to the 'netstat' command
 fi 
